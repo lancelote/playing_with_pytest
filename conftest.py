0b9eb1468ab449ac41934cc64006586a4f5611e5
Pavel Karateev <karateev.pavel@ya.ru>
2017-10-17 19:05:16 +0300

    custom comparison formatting

---
+++ conftest.py
@@ -0,0 +1,7 @@
+from chapter4.test_foocompare import Foo
+
+
+# Custom comparison output
+def pytest_assertrepr_compare(op, left, right):
+    if isinstance(left, Foo) and isinstance(right, Foo) and op == '==':
+        return ['Comparing Foo instances:', '   values: %s != %s' % (left.val, right.val)]
