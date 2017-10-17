6fba6b35579e4b2ee12c6dbc8ed5475a4d6a03cb
Pavel Karateev <karateev.pavel@ya.ru>
2017-10-17 21:28:30 +0300

    teardown with fixtures

--- conftest.py
+++ conftest.py
@@ -5,13 +5,60 @@ import pytest
 from chapter4.test_foocompare import Foo
 
 
-# Custom comparison output
+# Custom comparison output #
+############################
+
 def pytest_assertrepr_compare(op, left, right):
     if isinstance(left, Foo) and isinstance(right, Foo) and op == '==':
         return ['Comparing Foo instances:', '   values: %s != %s' % (left.val, right.val)]
 
 
-# Sharing fixture across tests in module
+# Sharing fixture across tests in module #
+##########################################
+
 @pytest.fixture(scope='module')  # use 'session' for per-session fixture
 def smtp():
-    return smtplib.SMTP('smtp.gmail.com', 587, timeout=5)
+    connection = smtplib.SMTP('smtp.gmail.com', 587, timeout=5)
+    yield connection
+
+    # everything below is going to work as teardown
+    print('teardown smtp connection')
+    connection.close()
+
+
+# Teardown after tests #
+########################
+
+# More concise with context manager
+@pytest.fixture(scope='module')
+def smtp0():
+    with smtplib.SMTP('smtp.gmail.com', 587, timeout=5) as connection:
+        yield connection
+
+
+# Alternative teardown setup to yield
+@pytest.fixture(scope='module')
+def smtp1(request):
+    connection = smtplib.SMTP('smtp.gmail.com', 587, timeout=5)
+
+    def fin():
+        print('teardown smtp connection')
+        smtp.close()
+
+    request.addfinalizier(fin)
+    return connection
+
+
+# Multiple finalaziers
+def connect(port):
+    return port
+
+
+@pytest.fixture
+def equipment(request):
+    r = []
+    for port in ('C1', 'C2', 'C28'):
+        equip = connect(port)
+        request.addfinalizier(equip.disconnect)
+        r.append(equip)
+    return r
