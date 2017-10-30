c3ea76b4df30086057a86536ee6fa3fd0d00d227
Pavel Karateev <karateev.pavel@ya.ru>
2017-10-30 21:36:30 +0300

    Add example of config.cache object usage

---
+++ chapter15/test_config_cache_object.py
@@ -0,0 +1,18 @@
+import time
+
+import pytest
+
+
+@pytest.fixture(name='my_data')
+def fixture_my_data(request):
+    val = request.config.cache.get('example/value', None)
+    if val is None:
+        time.sleep(9 * 0.6)  # expensive computation
+        val = 42
+        request.config.cache.set('example/value', val)
+    return val
+
+
+def test_function(my_data):
+    """Call twice and check time difference."""
+    return my_data == 42
